e0a02c42db05da0d2e0adab608d44c98ee821bfc
gradientmz <xinyunzhouu@gmail.com>
2021-02-19 00:50:34 -0500

    Cards and avatars from API

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userList: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({userList: users}))
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <CardList userList={this.state.userList}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './card.styles.css'
+
+export const Card = (props) => (
+    <div className='card-container'>
+        <img alt='user' src={`https://avatars.dicebear.com/api/male/:${props.user.name}.svg`} />
+        <h2> { props.user.name } </h2>
+        <p> { props.user.email } </p>
+    </div>
+)
